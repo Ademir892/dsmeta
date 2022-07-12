2eb5ee075c938571d687b1e3a47c1f02ff98ca4f
Ademir Marmitt Traesel <ademirtraesel@hotmail.com>
2022-07-12 20:16:49 -0300

    Datapicker

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,17 @@
-import NotificationBUtton from "./components/NotificationButton"
 import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
 
 function App() {
   return (
     <>
-      <Header/>
-      <NotificationBUtton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
   )
 }
